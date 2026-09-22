194c36bf049c9202fd317884b53b8c9c5445e8bd
Muhammad Nafis <nafisbjm06@gmail.com>
2026-09-22 21:12:42 +0800

    Menambahkan Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/halo/java/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01.halo.java;
+
+/**
+ *
+ * @author user
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+      String nama = "Muhammad Nafis";
+      long npm = 2410010629L;
+      String prodi = "Teknik Informatika";
+      int semester = 5;
+      String alasan = "Mendalami bahasa java";
+      
+        System.out.println("========================================");
+        System.out.println("\tKARTU MAHASISWA PBO 2");
+        System.out.println("========================================");
+        System.out.println("Nama \t\t: "+ nama );
+        System.out.println("NPM \t\t: "+ npm );
+        System.out.println("Prodi \t\t: "+ prodi );
+        System.out.println("Semester \t: "+ semester );
+        System.out.println("Alasan \t\t: "+ alasan );
+        System.out.println("========================================");
+    }}
+    
+
