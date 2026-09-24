503b973342de99ac66d238e662e48a5a3cb787c9
Muhammad Nafis <nafisbjm06@gmail.com>
2026-09-24 14:10:54 +0800

    P01: proyek pertama HaloPBO2

--- p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/HaloPBO2.java
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/HaloPBO2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package id.ac.uniska.pbo2.p01.halo.java;
+package id.ac.uniska.pbo2.p01;
 
 /**
  *
